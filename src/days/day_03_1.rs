f0237a897405fc0d009c3db8e4ada9d0c83ad262
mhillmann <mhillmann@uos.de>
2025-12-03 06:21:23 +0100

    Added day 03 part 1

--- src/days/day_03_1.rs
+++ src/days/day_03_1.rs
@@ -3,7 +3,7 @@ use crate::utils::*;
 
 pub fn run() {
     #[allow(unused_variables)]
-    let input = include_str!("../input/current.txt");
+    let input = include_str!("../../input/day_03.txt");
     // let input = "";
 
     let result = input
