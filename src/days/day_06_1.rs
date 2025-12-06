42055cc89263e7dff91a7a914508645686881e86
mhillmann <mhillmann@uos.de>
2025-12-06 09:16:05 +0100

    Added day 06 part 1

--- src/days/day_06_1.rs
+++ src/days/day_06_1.rs
@@ -3,7 +3,7 @@ use crate::utils::*;
 
 pub fn run() {
     #[allow(unused_variables)]
-    let input = include_str!("../input/current.txt");
+    let input = include_str!("../../input/day_06.txt");
     // let input = "";
 
     // let mut grid = _grid(input);
