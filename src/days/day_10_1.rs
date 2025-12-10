814b69c0a6d0662bce637539177b2516d3efac63
mhillmann <mhillmann@uos.de>
2025-12-10 08:39:52 +0100

    Added day 10 part 1

--- src/days/day_10_1.rs
+++ src/days/day_10_1.rs
@@ -20,7 +20,7 @@ fn parse_button(button: &str) -> Mask {
 
 pub fn run() {
     #[allow(unused_variables)]
-    let input = include_str!("../input/current.txt");
+    let input = include_str!("../../input/day_10.txt");
     //     let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
     // [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
     // [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
